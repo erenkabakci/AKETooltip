fe80643c65e058f0fd1d58cad12d34b6b1f24ab2
Eren Kabakci <Eren.Kabakci@monitise.com>
2014-11-04 23:23:51 +0200

    Initial Commit

---
+++ AKETooltip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKETooltip
+//
+//  Created by Eren Kabakci on 04/11/14.
+//  Copyright (c) 2014 Akeara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
